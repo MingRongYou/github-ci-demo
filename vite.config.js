9ba54c8f68e5fcf329c3cbb8d9aeead722724ba4
TWMingR <namova25@gmail.com>
2023-06-03 01:37:21 +0800

    feat: 🎸 modify vite config content

--- vite.config.js
+++ vite.config.js
@@ -13,6 +13,6 @@ export default defineConfig({
     }
   },
   build: {
-    assetsDir: 'dist'
+    assetsFileName: process.env.NODE_ENV === 'production' ? '[name].[contenthash].[ext]' : '[name].[ext]'
   }
 })
