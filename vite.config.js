e582853c4793bba6eafd39a6ceb517c7dc455278
TWMingR <namova25@gmail.com>
2023-06-03 00:22:31 +0800

    feat: 🎸 modify vite config content

--- vite.config.js
+++ vite.config.js
@@ -13,6 +13,12 @@ export default defineConfig({
     }
   },
   build: {
-    outDir: 'assets',
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/[name].js',
+        chunkFileNames: 'assets/[name].js',
+        assetFileNames: 'assets/[name].[ext]',
+      },
+    },
   },
 })
