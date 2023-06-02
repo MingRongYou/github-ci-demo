873168457c6fcf1fca2566c5f2d1c14e432ae51f
TWMingR <namova25@gmail.com>
2023-06-03 01:45:58 +0800

    feat: 🎸 modify vite config content

--- vite.config.js
+++ vite.config.js
@@ -6,13 +6,10 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: './github-ci-demo/',
+  // base: './github-ci-demo/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  build: {
-    assetsFileName: process.env.NODE_ENV === 'production' ? '[name].[contenthash].[ext]' : '[name].[ext]'
-  }
 })
