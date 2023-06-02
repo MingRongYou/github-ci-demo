700367cfb6bffadb653f3fbb7a3f1beef37156f9
TWMingR <namova25@gmail.com>
2023-06-03 03:29:55 +0800

    feat: 🎸 modify vite config content

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,22 @@
-import { fileURLToPath, URL } from 'node:url'
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { fileURLToPath, URL } from 'node:url';
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
-  base: process.env.VITE_BASE_URL,
+  plugins: [vue()],
+  base: '/github-ci-demo/',
+  build: {
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
+    }
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-})
+});
