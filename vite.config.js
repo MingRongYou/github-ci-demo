4d396c5d7677f67bd54ba5a709835d6486427772
TWMingR <namova25@gmail.com>
2023-06-03 02:55:28 +0800

    feat: 🎸 modify project plugins

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,21 @@
 import { fileURLToPath, URL } from 'node:url'
-
+import html from 'vite-plugin-html'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    html({
+      inject: {
+        injectData: {
+          baseURL: 'https://mingrongyou.github.io/github-ci-demo/'
+        }
+      }
+    })
+  ],
   base: '/github-ci-demo/',
-  build: {
-    // 设置 publicPath 为部署目标地址
-    publicPath: 'https://mingrongyou.github.io/github-ci-demo/',
-  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
